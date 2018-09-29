c908a0c3e84069e73cb2218755f84e4303d121c0
Vincent <const.lomo@gmail.com>
2018-09-29 15:02:15 +0800

    add: Layer.addElement/removeElement

--- src/Layer.js
+++ src/Layer.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { createPortal } from 'react-dom';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 /**
  * 层组件
+ * let instance = Layer.addElement(<Tooltip>);
+ * Layer.removeElement(instance);
  */
 
 export default class Layer extends React.Component {
+  static __layerRootContainer;
+  static __layerMap = new Map();
+  static get layerRootContainer() {
+    if (!Layer.__layerRootContainer) {
+      const container = document.body.appendChild(
+        document.createElement('div')
+      );
+      container.dataset.layerRoot = '';
+      Layer.__layerRootContainer = container;
+    }
+  }
+  static set layerRootContainer(container) {
+    Layer.__layerRootContainer = container;
+  }
+  static addElement(element) {
+    if (element) {
+      const layerRootContainer = Layer.layerRootContainer;
+      const container = layerRootContainer.appendChild(
+        document.createElement('div')
+      );
+      const instance = ReactDOM.render(element, container);
+      Layer.__layerMap.set(instance._reactInternalInstance, container);
+      return instance;
+    }
+  }
+
+  static removeElement(instance) {
+    if (instance && instance._reactInternalInstance) {
+      const container = Layer.__layerMap.get(instance._reactInternalInstance);
+      if (container) {
+        ReactDOM.unmountComponentAtNode(container);
+        if (container.parentNode) {
+          container.parentNode.removeChild(container);
+        }
+        Layer.__layerMap.delete(instance._reactInternalInstance);
+      }
+    }
+  }
+
   static propTypes = {
     children: PropTypes.node
   };
@@ -32,6 +73,6 @@ export default class Layer extends React.Component {
 
   render() {
     const { children } = this.props;
-    return createPortal(children, this.el);
+    return ReactDOM.createPortal(children, this.el);
   }
 }
