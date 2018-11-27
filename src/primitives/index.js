3776b40c120df37e8e388f6841feff074ca5a62e
Vincent <const.lomo@gmail.com>
2018-11-27 18:43:00 +0800

    add: inline & block

--- src/primitives/index.js
+++ src/primitives/index.js
@@ -8,7 +8,7 @@ export const Inline = factory('Inline', { display: 'inline' });
 export const InlineBlock = factory('InlineBlock', { display: 'inline-block' });
 export const InlineFlex = factory('InlineFlex', { display: 'inline-flex' });
 
-export const Text = factory('Text', { is: 'p', size: 'xxs' });
+export const Text = factory('Text', { is: 'div', size: 'xxs' });
 export const Input = factory('Input', { is: 'input', size: 'xxs' });
 export const Link = factory('Link', { is: 'a', size: 'xxs' });
 export const Button = factory('Button', { is: 'button', size: 'xxs' });
