c35ac19d6e19c368a61dd773766c5d5c32854656
Vincent <const.lomo@gmail.com>
2018-11-29 16:10:49 +0800

    add: webp

--- src/Image.js
+++ src/Image.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Box } from './primitives';
 
+const WebPEnabled = (function() {
+  const canvas = document.createElement('canvas');
+  canvas.width = 1;
+  canvas.height = 1;
+  return anvas.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+})();
+
 const Image = props => {
   let {
     src,
+    webp,
     contain = false,
     cover = false,
     fit = false,
@@ -21,7 +29,7 @@ const Image = props => {
       backgroundSize={
         contain ? 'contain' : cover ? 'cover' : fit ? '100% 100%' : 'unset'
       }
-      backgroundImage={`url('${src}')`}
+      backgroundImage={`url('${WebPEnabled ? webp || src : src}')`}
     />
   );
 };
